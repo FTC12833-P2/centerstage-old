81356c26b1bdfb5524d697feecd49f9d5e519fb3
ftc12833-p2 <ftc12833p2@gmail.com>
2023-10-17 17:53:06 -0400

    Control collector motors with triggers

--- src/main/java/CenterstageBOB/Collector.java
+++ src/main/java/CenterstageBOB/Collector.java
@@ -1,13 +1,13 @@
 package CenterstageBOB;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class Collector {
     private final LinearOpMode opMode;
 
-    private CRServo leftWheel = null;
-    private CRServo rightWheel = null;
+    private DcMotorEx leftWheel = null;
+    private DcMotorEx rightWheel = null;
 
     public Collector(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -15,12 +15,23 @@ public class Collector {
     }
 
     public void collect() {
-        if (opMode.gamepad2.right_bumper) {
-            leftWheel.setPower(-1);
-            rightWheel.setPower(-1);
-        } else if (opMode.gamepad2.left_bumper) {
-            leftWheel.setPower(1);
-            rightWheel.setPower(1);
+//        if (opMode.gamepad2.right_bumper) {
+//            leftWheel.setPower(-1);
+//            rightWheel.setPower(-1);
+//        } else if (opMode.gamepad2.left_bumper) {
+//            leftWheel.setPower(1);
+//            rightWheel.setPower(1);
+//        } else {
+//            leftWheel.setPower(0);
+//            rightWheel.setPower(0);
+//        }
+
+        if (opMode.gamepad2.right_trigger > 0.1) {
+            leftWheel.setPower(-opMode.gamepad2.right_trigger);
+            rightWheel.setPower(-opMode.gamepad2.right_trigger);
+        } else if (opMode.gamepad2.left_trigger > 0.1) {
+            leftWheel.setPower(opMode.gamepad2.left_trigger);
+            rightWheel.setPower(opMode.gamepad2.left_trigger);
         } else {
             leftWheel.setPower(0);
             rightWheel.setPower(0);
@@ -28,9 +39,9 @@ public class Collector {
     }
 
     public void init() {
-        leftWheel = opMode.hardwareMap.get(CRServo.class, "leftWheel");
-        rightWheel = opMode.hardwareMap.get(CRServo.class, "rightWheel");
+        leftWheel = opMode.hardwareMap.get(DcMotorEx.class, "leftWheel");
+        rightWheel = opMode.hardwareMap.get(DcMotorEx.class, "rightWheel");
 
-        rightWheel.setDirection(CRServo.Direction.REVERSE);
+        rightWheel.setDirection(DcMotorEx.Direction.REVERSE);
     }
 }
